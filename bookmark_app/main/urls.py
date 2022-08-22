7c7e079c85db258b0bdcbc919d6b7a81060662de
Frissse <awaumans@me.com>
2022-08-22 15:25:39 +0200

    log in added

--- bookmark_app/main/urls.py
+++ bookmark_app/main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('login/', views.loginPage, name='login'),
+  path('logout/', views.logoutUser, name='logout'),
+  
   path('', views.overview, name="overview"),
   path('add/', views.add, name='add-bookmark'),
   path('edit/<int:id>', views.update, name='edit-bookmark'),
--- bookmark_app/main/views.py
+++ bookmark_app/main/views.py
@@ -1,15 +1,51 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .models import Bookmarks, Tag
 from .forms import BookmarksForm, TagForm
 
 # Create your views here.
+def loginPage(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    try:
+      user = User.objects.get(username=username)
+    except:
+      messages.error(request, 'User does not exist')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      return redirect('overview')
+    else:
+      messages.error(request, 'User does not exist')
+
+  return render(request, 'main/login_register.html')
+
+def logoutUser(request):
+  logout((request))
+  return redirect('overview')
+
+
 def overview(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
   tags = Tag.objects.all()
-  bookmarks = Bookmarks.objects.filter(tags__name__icontains=q)
-  return render(request, 'main/overview.html', {'bookmarks':bookmarks, 'tags': tags})
+  bookmarks = Bookmarks.objects.filter(
+    Q(tags__name__icontains=q) |
+    Q(title__icontains=q) |
+    Q(url__contains=q)
+    )
+  bookmark_count = bookmarks.count()
+  return render(request, 'main/overview.html', {'bookmarks':bookmarks, 'tags': tags, 'bookmark_count': bookmark_count})
 
+@login_required(login_url='login')
 def add(request):
   form = BookmarksForm()
   if request.method == 'POST':
